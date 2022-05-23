27c111e4ed82e36295df4ee98d5135e354eebf3e
leylakhalilli <leylaxelilli65@gmail.com>
2022-05-23 09:14:07 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express()
+app.get('/', function (request, response) {
+    response.send("hi leyla")
+})
+
+app.get('/about', function (request, response) {
+    response.send("About Page")
+})
+
+app.get('/home', function (request, response) {
+    response.send('this is leyla')
+})
+
+app.get('/date', function (request, response) {
+    const newDate = new Date()
+    response.send(newDate.toLocaleTimeString())
+})
+
+
+app.listen(3000, function () {
+
+    console.log("localhost:3000")
+})
